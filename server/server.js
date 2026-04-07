b4402e3b5726cd9f8b93a26b3141e8b8951c28cf
Adarsh Kumar Mishra <am1494400@gmail.com>
2026-04-07 23:17:03 +0530

    frontend url

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 // ===== CORS (VERY IMPORTANT FOR COOKIES) =====
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000', 'http://localhost:5175'],
+    origin: ['https://drive-now-tau.vercel.app', 'http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000', 'http://localhost:5175'],
     credentials: true,
   })
 );
